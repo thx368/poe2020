eafd13f1f99b1a58e352d8dc261d672137ccd030
thusy <cleverage6@gmail.com>
2020-01-30 14:30:55 +0100

    tp5

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,22 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: 'src/index.html'
     })
-
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      },
+    ],
+  }
 };
 
 
